b4cf79eb90a4958a0db2e3c0ed16379149c9c4b7
rbrands <mail@robert-brands.com>
2020-05-28 10:56:39 +0200

    Sitemap and new CosmosDBRepository

---
+++ Repositories/PagedResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using robert_brands_com.Models;
+
+namespace robert_brands_com.Repositories
+{
+    public class PagedResult<T> where T : DocumentDBEntity
+    {
+        public string ContinuationToken { get; set; }
+        public List<T> Result { get; set; }
+
+    }
+}
